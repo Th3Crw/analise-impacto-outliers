87ece556bfd0e3d975986a6aa247266d8849d263
João Ubirajara <120143384+Th3Crw@users.noreply.github.com>
2024-11-20 17:24:35 -0300

    Formula Arrumada

--- identificar_outliers.py
+++ identificar_outliers.py
@@ -6,11 +6,14 @@ def outlier_por_desvio_padrao (dataset, coluna):
     column_as_list = dataset[coluna].to_list()
 
     desvio_padrao = np.std(column_as_list, ddof=0)
+    media = np.mean(column_as_list)
     outlier = []
-    fator_de_mult = 5
+    fator_de_mult = 3
 
     for dado in column_as_list: 
-        if dado >= (desvio_padrao*fator_de_mult):
+        if dado >= media + (desvio_padrao*fator_de_mult):
+            outlier.append(dado)
+        if dado <= media + (desvio_padrao*fator_de_mult) * -1:
             outlier.append(dado)
 
     return outlier
