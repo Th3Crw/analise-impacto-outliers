cc85ee9d631016f91d60297c9e21c337b03fa96e
João Ubirajara <120143384+Th3Crw@users.noreply.github.com>
2024-11-20 17:48:53 -0300

    Var name changes

--- identificar_outliers.py
+++ identificar_outliers.py
@@ -5,15 +5,15 @@ def outlier_por_desvio_padrao (dataset, coluna):
     
     column_as_list = dataset[coluna].to_list()
 
-    desvio_padrao = np.std(column_as_list, ddof=0)
-    media = np.mean(column_as_list)
+    standard_deviation = np.std(column_as_list, ddof=0)
+    mean = np.mean(column_as_list)
     outlier = []
-    fator_de_mult = 3
+    fator_multiplicacao = 3
 
-    for dado in column_as_list: 
-        if dado >= media + (desvio_padrao*fator_de_mult):
-            outlier.append(dado)
-        if dado <= media + (desvio_padrao*fator_de_mult) * -1:
-            outlier.append(dado)
+    for data in column_as_list: 
+        if data >= mean + (standard_deviation*fator_multiplicacao):
+            outlier.append(data)
+        if data <= mean + (standard_deviation*fator_multiplicacao) * -1:
+            outlier.append(data)
 
     return outlier
